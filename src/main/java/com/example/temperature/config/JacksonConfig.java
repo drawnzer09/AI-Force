7af75d767563cfac2a60b9d46cc84c13319dbe86
drawnzer09 <shubhamdaheriya626@gmail.com>
2026-07-07 23:25:32 +0530

    Add Jackson configuration

---
+++ src/main/java/com/example/temperature/config/JacksonConfig.java
@@ -0,0 +1,22 @@
+package com.example.temperature.config;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() {
+        return builder -> builder
+                .featuresToEnable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .featuresToDisable(
+                        SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,
+                        JsonParser.Feature.ALLOW_NON_NUMERIC_NUMBERS
+                );
+    }
+}
