2cdb027044c5061204979f197afc43081d76c8e2
drawnzer09 <shubhamdaheriya626@gmail.com>
2026-07-09 18:21:58 +0530

    Add Spring Boot application class

---
+++ src/main/java/com/example/temperature/TemperatureTimeseriesApplication.java
@@ -0,0 +1,12 @@
+package com.example.temperature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureTimeseriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemperatureTimeseriesApplication.class, args);
+    }
+}
