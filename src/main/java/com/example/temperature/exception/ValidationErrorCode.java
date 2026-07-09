613fcb7a66e89b61c0a1f6073288257fd6b1cd25
drawnzer09 <shubhamdaheriya626@gmail.com>
2026-07-09 18:29:59 +0530

    Add ValidationErrorCode

--- src/main/java/com/example/temperature/exception/ValidationErrorCode.java
+++ src/main/java/com/example/temperature/exception/ValidationErrorCode.java
@@ -2,9 +2,9 @@ package com.example.temperature.exception;
 
 public enum ValidationErrorCode {
     BAD_REQUEST,
-    VALIDATION_ERROR,
+    VALIDATION_FAILED,
     PAYLOAD_TOO_LARGE,
     UNSUPPORTED_MEDIA_TYPE,
     METHOD_NOT_ALLOWED,
-    INTERNAL_ERROR
+    INTERNAL_SERVER_ERROR
 }
