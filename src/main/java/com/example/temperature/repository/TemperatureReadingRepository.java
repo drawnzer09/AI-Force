63051e1872d7bf5f62d39a23a941046839dbe9e5
drawnzer09 <shubhamdaheriya626@gmail.com>
2026-07-09 18:22:20 +0530

    Add temperature reading repository

--- src/main/java/com/example/temperature/repository/TemperatureReadingRepository.java
+++ src/main/java/com/example/temperature/repository/TemperatureReadingRepository.java
@@ -1,4 +1,17 @@
 package com.example.temperature.repository;
 
-public interface TemperatureReadingRepository extends TemperatureReadingRepositoryCustom {
+import com.example.temperature.entity.TemperatureReadingEntity;
+import java.time.OffsetDateTime;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface TemperatureReadingRepository extends JpaRepository<TemperatureReadingEntity, Long>, JpaSpecificationExecutor<TemperatureReadingEntity> {
+
+    Page<TemperatureReadingEntity> findByReadingTimestampGreaterThanEqualAndReadingTimestampLessThan(
+            OffsetDateTime from,
+            OffsetDateTime to,
+            Pageable pageable
+    );
 }
