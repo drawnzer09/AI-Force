d8346effc88b8a12536124006a2ba659d0258066
drawnzer09 <shubhamdaheriya626@gmail.com>
2026-07-10 11:38:28 +0530

    Add error body DTO

--- src/main/java/com/example/temperature/dto/error/ErrorBody.java
+++ src/main/java/com/example/temperature/dto/error/ErrorBody.java
@@ -2,39 +2,9 @@ package com.example.temperature.dto.error;
 
 import java.util.List;
 
-public class ErrorBody {
-
-    private String code;
-    private String message;
-    private List<ErrorDetail> details;
-
-    public ErrorBody(String code, String message, List<ErrorDetail> details) {
-        this.code = code;
-        this.message = message;
-        this.details = details;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public List<ErrorDetail> getDetails() {
-        return details;
-    }
-
-    public void setDetails(List<ErrorDetail> details) {
-        this.details = details;
-    }
+public record ErrorBody(
+        String code,
+        String message,
+        List<ErrorDetail> details
+) {
 }
