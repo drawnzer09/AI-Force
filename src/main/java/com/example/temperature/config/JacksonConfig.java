f5082cb43f53938bb4ed2428f3b42e62749802fb
drawnzer09 <shubhamdaheriya626@gmail.com>
2026-07-10 11:39:29 +0530

    Add Jackson configuration

--- src/main/java/com/example/temperature/config/JacksonConfig.java
+++ src/main/java/com/example/temperature/config/JacksonConfig.java
@@ -1,15 +1,21 @@
 package com.example.temperature.config;
 
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
 
 @Configuration
 public class JacksonConfig {
 
     @Bean
-    public Jackson2ObjectMapperBuilderCustomizer javaTimeSerializationCustomizer() {
-        return builder -> builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() {
+        return builder -> builder
+                .modules(new JavaTimeModule())
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .timeZone(TimeZone.getTimeZone("UTC"));
     }
 }
