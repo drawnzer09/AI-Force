2b46471c70db11c8de8b1a1a9dc752b1644275bf
drawnzer09 <shubhamdaheriya626@gmail.com>
2026-07-10 14:17:19 +0530

    Add build & bootstrap files

---
+++ src/main/java/com/example/temperature/TemperatureTimeSeriesApplication.java
@@ -0,0 +1,12 @@
+package com.example.temperature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureTimeSeriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemperatureTimeSeriesApplication.class, args);
+    }
+}
