ad6ea3bb85905470c7d67596e440e9339361cd43
drawnzer09 <shubhamdaheriya626@gmail.com>
2026-07-13 14:11:28 +0530

    Update API layer

--- src/main/java/com/example/temperature/config/JpaConfig.java
+++ src/main/java/com/example/temperature/config/JpaConfig.java
@@ -1,9 +1,7 @@
 package com.example.temperature.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
 public class JpaConfig {
 }
