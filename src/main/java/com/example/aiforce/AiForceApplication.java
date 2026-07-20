b5eb95ed6915574f843427f83a62f6442c82b96e
drawnzer09 <shubhamdaheriya626@gmail.com>
2026-07-20 12:16:16 +0530

    Add build & bootstrap files

---
+++ src/main/java/com/example/aiforce/AiForceApplication.java
@@ -0,0 +1,12 @@
+package com.example.aiforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiForceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiForceApplication.class, args);
+    }
+}
