40c46a465ba13c621f43928e390ffc352cd39c17
drawnzer09 <shubhamdaheriya626@gmail.com>
2026-07-27 12:28:05 +0200

    Add build & bootstrap files

---
+++ src/main/java/com/example/temperature/TemperatureTimeseriesApplication.java
@@ -0,0 +1,15 @@
+package com.example.temperature;
+
+import com.example.temperature.config.ApplicationProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(ApplicationProperties.class)
+public class TemperatureTimeseriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemperatureTimeseriesApplication.class, args);
+    }
+}
